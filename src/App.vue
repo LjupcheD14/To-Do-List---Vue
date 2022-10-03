517f92b819b186862d11b312cd2b60cf37bbc9d7
Ljubomir Davitkov <ljubomir.davitkov@students.finki.ukim.mk>
2022-10-04 01:12:17 +0200

    Mark done

--- src/App.vue
+++ src/App.vue
@@ -5,42 +5,31 @@
       Bubo ToDo
     </div>
 
-    <form
-      @submit.prevent="addTodo"
-    >
+    <form @submit.prevent="addTodo">
       <div class="field is-grouped mb-5">
         <p class="control is-expanded">
-          <input
-          v-model="newTodoContent" 
-          class="input" 
-          type="text" 
-          placeholder="Add a todo">
+          <input v-model="newTodoContent" class="input" type="text" placeholder="Add a todo">
         </p>
         <p class="control">
-          <button 
-          :disabled="!newTodoContent"
-          class="button is-info"
-          >
+          <button :disabled="!newTodoContent" class="button is-info">
             Add
           </button>
         </p>
       </div>
     </form>
 
-    <div v-for="todo in todos" class="card mb-5">
+    <div v-for="todo in todos" class="card mb-5" :class="{'has-background-success-light' : todo.done}">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">
+            <div class="column" :class="{'has-text-success line-through' : todo.done}">
               {{todo.content}}
             </div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">
+              <button @click="toggleDone(todo.id)" class="button" :class="todo.done ? 'is-success' : 'is-light'">
                 &check;
               </button>
-              <button
-                @click="deleteTodo(todo.id)"
-               class="button is-danger ml-2">
+              <button @click="deleteTodo(todo.id)" class="button is-danger ml-2">
                 &cross;
               </button>
 
@@ -74,7 +63,7 @@ const todos = ref([
   {
     id: 'id2',
     content: 'Train basket',
-    done: false
+    done: true
   }
 ])
 
@@ -98,6 +87,14 @@ const deleteTodo = id => {
   todos.value = todos.value.filter(todo => todo.id !== id)
 }
 
+// toggle done
+
+const toggleDone = id => {
+  console.log('toggleDone', id)
+  const index = todos.value.findIndex(todo => todo.id === id)
+  todos.value[index].done = !todos.value[index].done
+}
+
 </script>
 
 <style>
@@ -107,5 +104,11 @@ const deleteTodo = id => {
   max-width: 400px;
   padding: 20px;
   margin: 0 auto;
+}
+.line-through{
+  text-decoration: line-through;
+}
+.has-background-success-light{
+  background-color: rgb(187, 255, 187) !important;
 }
 </style>
