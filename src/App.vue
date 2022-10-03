8e6b585b231162bfaf9771b28d4761c1c1d5c104
Ljubomir Davitkov <ljubomir.davitkov@students.finki.ukim.mk>
2022-10-03 03:12:51 +0200

    Elements, Styeles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,57 @@
 <template>
-  Ljupce 12
+
+  <div class="badass-todo">
+    <div class="title has-text-centered">
+      Bubo ToDo
+    </div>
+
+
+    <div class="field is-grouped mb-5">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="Add a todo">
+      </p>
+      <p class="control">
+        <a class="button is-info">
+          Add
+        </a>
+      </p>
+    </div>
+
+    <div
+    v-for="i in 3"
+     class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">
+              Train karate
+            </div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">
+                &check;
+              </button>
+              <button class="button is-danger ml-2">
+                &check;
+              </button>
+
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </template>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+
+.badass-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
