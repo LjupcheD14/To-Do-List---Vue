5b059ee6ab568da172bacb0ca3fa614d92a75e6f
Ljubomir Davitkov <ljubomir.davitkov@students.finki.ukim.mk>
2022-10-04 23:12:52 +0200

    Delete todo

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@
 // imports
 
 import { ref, onMounted } from 'vue';
-import { collection, onSnapshot, addDoc } from 'firebase/firestore';
+import { collection, onSnapshot, addDoc, doc, deleteDoc } from 'firebase/firestore';
 import { db } from '@/firebase';
 
 // firebase ref
@@ -119,7 +119,7 @@ const addTodo = () => {
 // delete todo
 
 const deleteTodo = id => {
-  todos.value = todos.value.filter(todo => todo.id !== id)
+  deleteDoc(doc(todosCollectionRef, id));
 }
 
 // toggle done
